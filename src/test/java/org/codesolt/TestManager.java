8079a3bbcf8b19d15086fb7f4defa0222483deda
Carlos Salazar <carlos.salazar@codesolt.com>
2018-01-29 21:00:28 -0600

    Testing inited

---
+++ src/test/java/org/codesolt/TestManager.java
@@ -0,0 +1,37 @@
+package org.codesolt;
+
+import org.junit.Test;
+
+public class TestManager {
+
+	/* 
+	 * For PROD applications,
+	 * test the following operations 
+	 * pointing to QA or DEV only
+	*/
+	
+	@Test
+	public void ensureGetByUsername() {
+		
+	}
+	
+	@Test
+	public void ensureGetAll() {
+		
+	}
+	
+	@Test
+	public void ensureCreateUser() {
+		
+	}
+
+	@Test
+	public void updateUser() {
+		
+	}
+	
+	@Test
+	public void ensureDeleteUser() {
+		
+	}
+}
---
+++ src/test/java/org/codesolt/TestRepository.java
@@ -0,0 +1,42 @@
+package org.codesolt;
+
+import org.junit.Test;
+
+public class TestRepository {
+
+	/* 
+	 * For PROD applications,
+	 * test the following operations 
+	 * pointing to QA or DEV only
+	*/
+	
+	@Test
+	public void ensureFindIdByUsername() {
+		
+	}
+
+	@Test
+	public void ensureFindByUsername() {
+		
+	}
+	
+	@Test
+	public void ensureFindAll() {
+		
+	}
+	
+	@Test
+	public void ensureCreateUser() {
+		
+	}
+
+	@Test
+	public void updateUser() {
+		
+	}
+	
+	@Test
+	public void ensureDeleteUser() {
+		
+	}	
+}
